8370e2f5c79ce04717b3bf92c21a314021ee1c26
Anton Gordeyev <ufoxix@gmail.com>
2020-04-27 21:15:07 +0300

    Prepare to service contracts workshop

---
+++ app/code/Vitaliy/ServiceContract/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Vitaliy_ServiceContract',
+    __DIR__
+);
---
+++ app/code/Vladislav/ServiceContract/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Vladislav_ServiceContract',
+    __DIR__
+);
